398cf44a7fa16acaa0e8931f6624d6afdcadadd2
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-05-28 12:25:56 -0700

    add user creation form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UserCreate />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import UserCreate from '@/components/UserCreate.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    UserCreate
   }
 }
 </script>
